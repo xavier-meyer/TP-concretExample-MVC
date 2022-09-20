a56a1c63049eedc6ed6dbede947db6ec8d1c7902
xavier-meyer <xavier.01@orange.fr>
2022-09-20 20:12:41 +0200

    feat: folder index.php + .htaccess + Autoloader + Router + RouterException

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
 <?php
+
+require_once 'Autoloader.php';
+require_once 'config/config.php';
+
+use App\Autoloader;
+use App\Controller\HomeController;
+use App\Exception\RouterException;
+use App\Service\Router;
+
+Autoloader::$folderList =
+    [
+        "App\Entity",
+        "App/Controller/",
+        "App/Repository/",
+        "App/Service/",
+        "App/Exception/",
+        "App/Form/",
+        "App/Validator/",
+    ];
+AutoLoader::register();
+
+//intégrant le systeme de Router.php qui permet de lancer une méthode
+//d'un controlleur en fonction de URL soumis au navigateur
+
+session_start();
+
+try {
+    $router = new Router($_GET['url']);
+
+    $router->get('/', function () {
+        echo (new HomeController())->invoke();
+    });
+
+//    $router->post('/add', function () {
+//        echo (new HomeController())->add();
+//    });
+
+    $router->run();
+} catch (RouterException|Exception $e) {
+    die('Error: ' . $e);
+}
+
+
